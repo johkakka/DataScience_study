1bdebabfef408f1cc064ca4fe4111e9d969e5799
yu-dell <kuutanmokeke@gmail.com>
2021-12-01 11:46:12 +0900

    update check_data.py

--- check_data.py
+++ check_data.py
@@ -1,15 +1,12 @@
 import pandas as pd
 
-df = pd.read_csv('hotel_bookings.csv')
+df = pd.read_csv('data/hotel_bookings.csv')
 
-#print(df.isnull().sum())
-#null = children4, country488, agent16340, company112593
-#children null=0, country null = NNN   agent null = -1, company なくす
+
 df_fill = df.drop('company', axis=1)
-
-df_fill = pd.Series([0, 'NNN', -1], index=['children', 'country','agent'])
-print(df_fill.isnull().sum())
-
+df_fill =df_fill.fillna({'children':0, 'country':'NNN', 'agent':-1})
+#print(df.isnull().all())
+print(df_fill)
 
 
 
